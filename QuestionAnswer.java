30767df23403c3a2163fbe084bd12c230c0c3c95
Ritik <111485308+ritik2a@users.noreply.github.com>
2023-07-23 19:39:54 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,75 @@
+package com.example.myindianquizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Q1. Who is the founder of Instagram?",
+            "Q2. Who is the brand ambassador of leading sports wear brand NIKE?",
+            "Q3. Who is the founder of PAYTM?",
+            "Q4. Which among the following is the largest Bootstrap Indian startup in terms of profitability?",
+            "Q5. Which type of Business Model does India's most wearable brand BOAT follow?",
+            "Q6. Which category of business does BHARATPE comes in?",
+            "Q7. Which is the largest fast food restaurant chain in the world?",
+            "Q8. Who is Ashneer Grover?",
+            "Q9. Which of the following are the duopoly businesses from India which are also originated in India only? ",
+            "Q10. Which new startup is giving tough competition to BigBasket and Blinkit by guaranteeing 10 minutes delivery of Grocery items? ",
+            "Q11. Which Airline has banned the use of single-use plastic on the flight since October 2019?",
+            "Q12. Which Indian bank has tied up with Paisabazaar.com to offer conditionally approved loans to the latter’s existing customers?",
+            "Q13. Recently Sequoia Capitals, which is VC firm, has changed its name of which of it's partenering country's firm?",
+            "Q14. Which of the following comes under the 4 P's of marketing?",
+            "Q15. IPO is an abbreviation for?",
+            "Q16. FPO is an abbreviation for?",
+            "Q17. Who is the founder of Flipkart?",
+            "Q18. Which of the following is the correct factor for determining valuation of a company?",
+            "Q19. Which Indian Bank has the largest market share in Credit Cards distribution in India?",
+            "Q20. Which are the important factors for a business to succeed?"
+
+    };
+
+    public static String choices[][]={
+            {"Mark Zuckerberg","Elon Musk","Jeff Bezos","Kevin Systrom"},
+            {"Lionel Messi","Cristiano Ronaldo","Virat Kohli","Yuvraj Singh"},
+            {"Sachin Bansal","Ashneer Grover","Vijay Shekhar Sharma","Sameer Nigam"},
+            {"BYUJ'S","Unacademy","Zerodha","Paytm"},
+            {"B2C","D2C","C2C","B2B"},
+            {"EdTech", "AgroTech", "FinTech", "FoodTech"},
+            {"Domino's","McDonald's","KFC","Starbucks"},
+            {"Television Personality","Banker","Entrepreneur","Influencer"},
+            {"Bata and Nike","Adidas and Puma", "Zomato and Swiggy", "Uber and Ola"},
+            {"MilkBasket","BlueSmart","Zepto","Pincode"},
+            {" Go AIr"," Indigo","Spice Jet","Air India"},
+            {"Dena Bank","Yes Bank","ICICI Bank","SBI Bank"},
+            {"Sequoia China","Sequoia India","Sequoia US","Sequoia UK"},
+            {"Product","Price","Place","All of the above"},
+            {"Initial Public Offering","Initial Price Offering","Initial Private Offering","Initial Product Offering"},
+            {"Follow-up Public Offering","Follow-on Public Offering","Full Public Offering","Final Public Offering"},
+            {"Sameer Nigam","Ashneer Grover","Sanjeev Bikchandani","Sachin Bansal"},
+            {"P/E Ratio","EBIT","P/B Ratio","EBITDA"},
+            {"ICICI Bank","SBI Bank","HDFC Bank","Axis Bank"},
+            {"Great Product, Higher Valuations, Great Cash Flow","Higher Valuations, Bigger Investors, Great Marketing Strategy","Great Product, Great Marketing Strategy, Higher Valuations", "Great Product, Happy Customers, Maximum Profit"}
+
+    };
+
+    public static String correctAnswers[]={
+            "Kevin Systrom",
+            "Cristiano Ronaldo",
+            "Vijay Shekhar Sharma",
+            "Zerodha",
+            "D2C",
+            "FinTech",
+            "McDonald's",
+            "Entrepreneur",
+            "Zomato and Swiggy",
+            "Zepto",
+            "Air India",
+            "Yes Bank",
+            "Sequoia India",
+            "All of the above",
+            "Initial Public Offering",
+            "Follow-on Public Offering",
+            "Sachin Bansal",
+            "EBITDA",
+            "HDFC Bank",
+            "Great Product, Happy Customers, Maximum Profit"
+    };
+}
